66ec5399fb556b12bc7e84daf9b485787c11b360
Paulo Cesar <paulocesar.araujo.ads@gmail.com>
2020-07-27 21:42:33 -0300

    Projeto Criado

---
+++ src/test/java/com/pc/mongosb/MongoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pc.mongosb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
