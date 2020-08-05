887fea368018a893f2be279c74748bfa506ca619
Paulo Cesar <paulocesar.araujo.ads@gmail.com>
2020-08-05 14:41:30 -0300

    Conectando ao MongoDB com repo e service

--- src/main/java/com/pc/mongosb/resource/UserResource.java
+++ src/main/java/com/pc/mongosb/resource/UserResource.java
@@ -4,23 +4,28 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pc.mongosb.domain.User;
+import com.pc.mongosb.service.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	
+	@Autowired
+	private UserService service;
 
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
-		User paulo = new User("1", "Paulo César", "pc@pc.com.br");
-		User thais = new User("2", "Thais Rocha", "thais@pc.com.br");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(paulo, thais));
+//		User paulo = new User("1", "Paulo César", "pc@pc.com.br");
+//		User thais = new User("2", "Thais Rocha", "thais@pc.com.br");
+		List<User> list = service.findAll();
+//		list.addAll(Arrays.asList(paulo, thais));
 		return ResponseEntity.ok().body(list);
 	}
 }
